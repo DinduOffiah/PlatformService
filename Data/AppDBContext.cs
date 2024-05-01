f61f8a452c4236d13ec047df2d300ef5112cf7a6
Chidindu <chidinduoffiah@gmail.com>
2024-05-01 13:40:07 +0100

    Added DB Context

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options)
+          : base(options)
+        {
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
