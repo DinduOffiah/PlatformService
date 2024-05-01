1af29b0deceba5deceac0814810cffbd5f2facc3
Chidindu <chidinduoffiah@gmail.com>
2024-05-01 15:01:54 +0100

    Added PlatformRead Dto

---
+++ Data/PrepDb.cs
@@ -0,0 +1,35 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using(var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDBContext>());
+            }
+        }
+
+        private static void SeedData(AppDBContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("Seeding Data...");
+
+                context.Platforms.AddRange(
+                    new Platform() { Name="Dot Net", Publisher="Microsoft", Cost="Free"},
+                    new Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "Kubernets", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
+                    );
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("Data already exist...");
+            }
+        }
+    }
+}
